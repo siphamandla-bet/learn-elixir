a1afedab836757e21176e384606f17281d19036f
Siphamandla Mzobe <siphamandla.mzobe@betsoftware>
2024-07-09 08:05:41 +0200

    added private functions

--- Basics/basics.exs
+++ Basics/basics.exs
@@ -1,15 +1,20 @@
 defmodule Calculator do
   def add x, y do
-    x + y
+    private_add(x, y)
   end
 
   def subtract x ,y do
-    x - y
+    private_subtract(x, y)
   end
+
+  defp private_subtract(x, y), do: x - y
+
+  defp private_add(x, y), do: x + y
+
 end
 
-sum = Calculator.add(1,3)
+sum = Calculator.add(2,3)
 IO.puts("Sum: #{sum}")
 
-diff = Calculator.subtract(5,3)
+diff = Calculator.subtract(7,3)
 IO.puts("Diff:  #{diff}")
