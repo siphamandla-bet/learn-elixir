12d44940e50d1ee30f1410f87c5bfb247a4dd692
Siphamandla Mzobe <siphamandla.mzobe@betsoftware>
2024-07-09 09:09:30 +0200

    lasagna exercise

--- Basics/basics.exs
+++ Basics/basics.exs
@@ -18,3 +18,31 @@ IO.puts("Sum: #{sum}")
 
 diff = Calculator.subtract(7,3)
 IO.puts("Diff:  #{diff}")
+
+
+defmodule Lasagna do
+  def expected_minutes_in_oven(), do: 40
+
+  def remaining_minutes_in_oven(time_in_oven), do: expected_minutes_in_oven() - time_in_oven
+
+  def preparation_time_in_minutes(number_layers) do
+    prep_time = 2
+    number_layers * prep_time
+  end
+
+  def total_time_in_minutes(number_of_layers, time_in_oven) do
+    preparation_time_in_minutes(number_of_layers) + time_in_oven
+  end
+
+  def alarm(), do: "Ding!"
+end
+
+IO.puts("Expected time in oven #{Lasagna.expected_minutes_in_oven()}")
+
+IO.puts("Remaining time in oven #{Lasagna.remaining_minutes_in_oven(30)}")
+
+IO.puts("Remaining time in oven #{Lasagna.preparation_time_in_minutes(2)}")
+
+IO.puts("total time in oven #{Lasagna.total_time_in_minutes(1, 30)}")
+
+IO.puts(Lasagna.alarm())
